41ca51c4f05fd30fe3733688baa3c1ae153fee5e
amitch6097 <mitcan@Andrew-Mitchells-MacBook-Pro.local>
2020-01-04 14:48:04 -0500

    home page w/ bio update

---
+++ src/components/Bio/Bio.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Bio.less';
+
+export default function Bio({}) {
+    return (
+        <div className="bio">
+            <div className="bio__name">
+                <h1 className="bio__first-name">Andrew</h1>
+                <h1 className="bio__last-name">Mitchell</h1>
+            </div>
+            <img className="bio__image" src="https://i.imgur.com/xtoLyW2.jpg" />
+            <h2 className="bio__role">Software Engineer</h2>
+            <div className="bio__links">
+                <ul className="bio__links-list">
+                    <li className="bio__links-list-item">
+                        <a className="link-button">Projects</a>
+                    </li>
+                    <li className="bio__links-list-item">
+                        <a
+                            className="link-button"
+                            href="https://github.com/amitch6097"
+                        >
+                            Resume
+                        </a>
+                    </li>
+                    <li className="bio__links-list-item">
+                        <a
+                            className="link-button"
+                            href="https://github.com/amitch6097"
+                        >
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,65 +1,29 @@
 import React from 'react';
-import P5Frame from '../../components/P5Frame/P5Frame';
 import './Home.less';
+
+import P5Frame from '../../components/P5Frame/P5Frame';
+import Bio from '../../components/Bio/Bio';
 
 export default function Home() {
     return (
         <div className="home-page">
-            <div className="home-page__title">Andrew Mitchell</div>
-            <div className="home-page__iframe">
-                <P5Frame
-                    width={'100%'}
-                    height={'100%'}
-                    frameURL={
-                        'https://editor.p5js.org/mitch702/embed/p6vC1kfsP'
-                    }
-                    linkURL={
-                        'https://editor.p5js.org/mitch702/sketches/p6vC1kfsP'
-                    }
-                />
-            </div>
-            <header className="Sndrew-header">
-                <div className="Sndrew-header__bio">
-                    <div className="Sndrew-header__bio-name">
-                        <h1 className="Sndrew-header__bio-first-name">
-                            Andrew
-                        </h1>
-                        <h1 className="Sndrew-header__bio-last-name">
-                            Mitchell
-                        </h1>
-                    </div>
-                    <img
-                        className="Sndrew-header__bio-image"
-                        src="https://i.imgur.com/xtoLyW2.jpg"
+            <div className="home-page__header">
+                <div className="home-page__header-iframe">
+                    <P5Frame
+                        width={'100%'}
+                        height={'100%'}
+                        frameURL={
+                            'https://editor.p5js.org/mitch702/embed/p6vC1kfsP'
+                        }
+                        linkURL={
+                            'https://editor.p5js.org/mitch702/sketches/p6vC1kfsP'
+                        }
                     />
-                    <h2 className="Sndrew-header__bio-role">
-                        Software Engineer
-                    </h2>
-                    <div className="Sndrew-header__bio-links">
-                        <ul className="Sndrew-header__bio-links-list">
-                            <li className="Sndrew-header__bio-links-list-item">
-                                <a className="link-button">Projects</a>
-                            </li>
-                            <li className="Sndrew-header__bio-links-list-item">
-                                <a
-                                    className="link-button"
-                                    href="https://github.com/amitch6097"
-                                >
-                                    Resume
-                                </a>
-                            </li>
-                            <li className="Sndrew-header__bio-links-list-item">
-                                <a
-                                    className="link-button"
-                                    href="https://github.com/amitch6097"
-                                >
-                                    Github
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-            </header>
+                <div className="home-page__header-bio">
+                    <Bio />
+                </div>
+            </div>
         </div>
     );
 }
